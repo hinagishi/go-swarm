01f41d7284fe630c8e5fdec5c8c5af5c1d3ae6cb
hinagishi <archangel.ratziel@gmail.com>
2016-01-13 19:02:15 +0900

    wip

--- swarm/swarm.go
+++ swarm/swarm.go
@@ -5,3 +5,21 @@ type Oauth struct {
 	ResponseType string
 	RedirectURI  string
 }
+
+func getCheckIns(v *url.Values) *Resjson {
+	url := "https://api.foursquare.com/v2/users/self?" + v.Encode()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	result := make(Resjson)
+	json.Unmarshal(body, result)
+}
